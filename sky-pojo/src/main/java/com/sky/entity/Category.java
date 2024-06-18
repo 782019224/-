2064b5dd2e91226b35a44d53e286b28bb3d50edd
jeff <jeff@outlook.com>
2024-06-18 10:38:26 +0800

    员工分页查询业务代码开发

--- sky-pojo/src/main/java/com/sky/entity/Category.java
+++ sky-pojo/src/main/java/com/sky/entity/Category.java
@@ -32,6 +32,7 @@ public class Category implements Serializable {
     //创建时间
     private LocalDateTime createTime;
 
+
     //更新时间
     private LocalDateTime updateTime;
 
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -29,4 +31,11 @@ public interface EmployeeMapper {
             "    #{createUser}, #{updateUser}, #{status}" +
             ");")
     void insert(Employee employee);
+
+
+    /*
+    * 分页查询的方法
+    * */
+
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
